183e1a7f935f2f99467a49c5a44d86e459c257a8
HONEY PATEL <123378526+honey-07@users.noreply.github.com>
2023-09-04 22:29:21 +0530

    Update que11.cpp

--- que11.cpp
+++ que11.cpp
@@ -1,1 +1,16 @@
 
+// Write a C++ program to enter a string and find its length.
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string name;
+
+	cout<<"Enter your name : ";
+	getline(cin,name);
+
+	cout << "The length of your name "<<name <<" is "<< name.length() << endl;
+
+return 0;
+}
